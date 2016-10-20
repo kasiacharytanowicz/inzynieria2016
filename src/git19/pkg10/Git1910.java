fd57d9e3a0d450935456af9af8079fb5820f34df
student <>
2016-10-20 18:26:56 +0200

    sprawdzam czy dziala. Daj znac czy tak hhaha

--- src/git19/pkg10/Git1910.java
+++ src/git19/pkg10/Git1910.java
@@ -14,6 +14,6 @@ public class Git1910 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Czesc piekna :) ");
     }
 }
